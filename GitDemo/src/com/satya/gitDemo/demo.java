554634d366d0a268154ba66b820820f48b626014
satyakam.m <satyakam.mohapatra91@gmailcom>
2016-09-15 18:54:56 +0530

    new line added

--- GitDemo/src/com/satya/gitDemo/demo.java
+++ GitDemo/src/com/satya/gitDemo/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hello World");
 		System.out.println("hello satya");
-		//removed the stg
+		System.out.println("new one");
 		System.out.println("hello git in my local");
 		demoBranch branch = new demoBranch();
 		branch.satya();
