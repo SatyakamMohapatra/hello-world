64cd94b829b95d2d588c7109ce6eb55f23b1b5f8
satyakam.m <satyakam.m@VDI-DT-DBSUP-01.SPI.COM>
2016-09-15 17:33:19 +0530

    i added sakshi

--- GitDemo/src/com/satya/gitDemo/demo.java
+++ GitDemo/src/com/satya/gitDemo/demo.java
@@ -5,6 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		System.out.println("hello World");
 		System.out.println("hello satya");
+		System.out.println("hello sakshi");
 		
 	}
 	}
